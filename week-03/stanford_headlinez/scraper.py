27bb0111da904fe6fab55c46b4b8a28ee3af5ef0
mmclaug <35282090+mmclaug@users.noreply.github.com>
2018-01-24 13:27:26 -0800

    Add files via upload

--- week-03/stanford_headlinez/scraper.py
+++ week-03/stanford_headlinez/scraper.py
@@ -5,18 +5,19 @@ def print_hedz(url='https://wgetsnaps.github.io/stanford-edu-news/news/'):
        hedtxt = extract_headline_text(t)
        print(hedtxt)
 def extract_headline_text(txt):
-    mystr = parse_headline_tags(txt)
-    xlist = mystr.split('>')
+   return txt.split('>')[2].split('<')[0]
+
 def parse_headline_tags(txt):
-    fetch_html(url)
     myList = []
+    fulltext = htm_fetch()
     lines = fetch_html.splitlines()
     for line in lines:
         if '<h3><a' in line:
             myList.append(line)
     return myList
+
 def fetch_html(url):
     import requests
     url = 'https://wgetsnaps.github.io/stanford-edu-news/news/'
     resp=requests.get(url)
-    print(resp.text)
+    return(resp.text)
